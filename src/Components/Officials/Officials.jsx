70d3d3269cbf57e6d12c77bde63bc7b4c8a4ee3c
Hasibur Rahman <hasibur15-2608@diu.edu.bd>
2023-09-12 15:25:58 +0600

    ok

--- src/Components/Officials/Officials.jsx
+++ src/Components/Officials/Officials.jsx
@@ -17,41 +17,90 @@ export default function Officials() {
           </thead>
           <tbody>
             <tr>
-              <th scope="row">1</th>
-              <td>জয়ন্তর বাপ</td>
-              <td>সভাপতি</td>
+              <th scope="row">১</th>
+              <td>রমাপদ রায়</td>
+              <td>প্রধান শিক্ষক (ভারপ্রাপ্ত)</td>
               <td>দেয়া যাবে না</td>
             </tr>
             <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
+              <th scope="row">২</th>
+              <td>মোঃ তাফাজ্জুল হোসেন</td>
+              <td>সহকারী শিক্ষক</td>
               <td>@fat</td>
             </tr>
             <tr>
-              <th scope="row">3</th>
-              <td>Larry</td>
-              <td>the Bird</td>
+              <th scope="row">৩</th>
+              <td>মোঃ কামরুল হাসান</td>
+              <td>সহকারী শিক্ষক</td>
               <td>@twitter</td>
             </tr>
             <tr>
-              <th scope="row">4</th>
-              <td>Larry</td>
-              <td>the Bird</td>
+              <th scope="row">৪</th>
+              <td>মোঃ আব্দুল আজিজ সরকার</td>
+              <td>সহকারী শিক্ষক</td>
               <td>@twitter</td>
             </tr>
             <tr>
-              <th scope="row">5</th>
-              <td>Larry</td>
-              <td>the Bird</td>
+              <th scope="row">৫</th>
+              <td>মোঃ আব্দুচ্ছালাম </td>
+              <td>সহকারী শিক্ষক</td>
               <td>@twitter</td>
             </tr>
             <tr>
-              <th scope="row">6</th>
-              <td>Larry</td>
-              <td>the Bird</td>
+              <th scope="row">৬</th>
+              <td>মোছাঃ রেহানা বেগম</td>
+              <td>সহকারী শিক্ষক</td>
               <td>@twitter</td>
             </tr>
+            <tr>
+              <th scope="row">৭</th>
+              <td>সকাল চন্দ্র রায়</td>
+              <td>সহকারী শিক্ষক</td>
+              <td>@twitter</td>
+            </tr>
+            <tr>
+              <th scope="row">৮</th>
+              <td>বিনীতা রাণী রায় </td>
+              <td>সহকারী শিক্ষক</td>
+              <td>@twitter</td>
+            </tr>
+            <tr>
+              <th scope="row">৯</th>
+              <td>মোঃ হাবিবুর রহমান</td>
+              <td>সহকারী শিক্ষক</td>
+              <td>@twitter</td>
+            </tr>
+            <tr>
+              <th scope="row">১০</th>
+              <td>সুধেন চন্র রায়</td>
+              <td>সহকারী শিক্ষক</td>
+              <td>@twitter</td>
+            </tr>
+            <tr>
+              <th scope="row">১১</th>
+              <td>মোছাঃ জাকিয়া জান্নাতি</td>
+              <td>সহকারী শিক্ষক</td>
+              <td>@twitter</td>
+            </tr>
+            <tr>
+              <th scope="row">১২</th>
+              <td>মোঃ আবদুস ছালাম</td>
+              <td>নিম্নমান সহকারী কাম কম্পিউটার</td>
+              <td>@twitter</td>
+            </tr>
+            <tr>
+              <th scope="row">১৩</th>
+              <td>মোঃশিদুল ইসলাম</td>
+              <td>দপ্তরী</td>
+              <td>@twitter</td>
+            </tr>
+            <tr>
+              <th scope="row">১৪</th>
+              <td>মোঃ আব্দুল কুদ্দুস</td>
+              <td>নৈশ প্রহরী</td>
+              <td>@twitter</td>
+            </tr>
+
           </tbody>
         </Table>
       </div>
