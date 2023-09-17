62d62e4840cbe6aae20ea8dde35fc7da4382de48
Hasibur Rahman <hasibur15-2608@diu.edu.bd>
2023-09-17 15:01:47 +0600

    oo

--- src/Components/LogIN/LogIN.jsx
+++ src/Components/LogIN/LogIN.jsx
@@ -1,11 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
+import './login.css'
+import { Button, FormGroup, Label, Input } from "reactstrap";
 
 export class LogIN extends Component {
   render() {
     return (
-      <div>LogIN</div>
-    )
+      <div style={{ paddingTop: 100, paddingLeft: 100, paddingRight: 100 }}>
+        <div className="bb">
+          <FormGroup>
+            <Label for="exampleEmail">Email</Label>
+            <Input
+              id="exampleEmail"
+              name="email"
+              placeholder="Enter your email"
+              type="email"
+              style={{backgroundColor:'skyblue',color:'white',}}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="examplePassword">Password</Label>
+            <Input
+              id="Password"
+              name="password"
+              placeholder="Enter your password"
+              type="password"
+              style={{backgroundColor:'skyblue',color:'white',}}
+            />
+          </FormGroup>
+          <Button style={{alignItems:'end',justifyContent:'end',backgroundColor:"green"}} color="primary">Log In</Button>
+        </div>
+      </div>
+    );
   }
 }
 
-export default LogIN
+export default LogIN;
