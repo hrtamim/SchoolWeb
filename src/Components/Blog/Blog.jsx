be0e1e8af7ea380d4bb0d43a66023d46cbe6275f
Hasibur Rahman <hasibur15-2608@diu.edu.bd>
2024-02-18 21:14:51 +0600

    ok

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
-import "./blog.css";
+// import React from 'react';
+// import "./blog.css";
 
 
-function Blog() {
+// function Blog() {
+//   return (
+//     <div className='container'>Blog</div>
+//   )
+// }
+
+// export default Blog
+import React, { useState } from 'react';
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
+import PropTypes from 'prop-types';
+
+function Blog({ direction, ...args }) {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggle = () => setDropdownOpen((prevState) => !prevState);
+
   return (
-    <div className='container'>Blog</div>
-  )
+    <div className="d-flex p-5">
+      <Dropdown isOpen={dropdownOpen} toggle={toggle} direction={direction}>
+        <DropdownToggle caret>Dropdown</DropdownToggle>
+        <DropdownMenu {...args}>
+          <DropdownItem>Header</DropdownItem>
+          <DropdownItem>Some Action</DropdownItem>
+          
+          <DropdownItem>Bar Action</DropdownItem>
+          <DropdownItem>Quo Action</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </div>
+  );
 }
 
-export default Blog
+Blog.propTypes = {
+  direction: PropTypes.string,
+};
+
+export default Blog;
